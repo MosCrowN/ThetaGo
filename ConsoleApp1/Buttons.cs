5b25343206d6f800897becfb8e3963b84931339d
Antony <conqueror.v.v.v.v.v@gmail.com>
2023-03-04 18:10:36 +0300

    Button class added

--- ConsoleApp1/Buttons.cs
+++ ConsoleApp1/Buttons.cs
@@ -3,39 +3,54 @@ using SFML.System;
 
 namespace ConsoleApp1;
 
+internal class Button : Drawable
+{
+    public Sprite? S;
+
+    public RectangleShape? R;
+
+    public bool IsSlider;
+    
+    public void Draw(RenderTarget target, RenderStates states)
+    {
+        target.Draw(S);
+        if (IsSlider)
+            target.Draw(R);
+    }
+}
+
 internal class ButtonLayout: Drawable
 {
-    public Sprite[]? Buttons;
-
-    public RectangleShape?[]? Sliders;
+    public Button[]? Buttons;
 
     private int _selected = -1;
 
     public int Selected(int x, int y, bool isPressed)
     {
         if (Buttons == null) return -1;
+        _selected = -1;
         for (var i = 0; i < Buttons.Length; ++i)
         {
-            if (x < Buttons[i].Position.X || x > (Buttons[i].Position.X + Buttons[i].Texture.Size.X * Buttons[i].Scale.X) ||
-                y < Buttons[i].Position.Y || y > (Buttons[i].Position.Y + Buttons[i].Texture.Size.Y * Buttons[i].Scale.Y)) 
+            if (x < Buttons[i].S!.Position.X || x > (Buttons[i].S!.Position.X + Buttons[i].S!.Texture.Size.X * Buttons[i].S!.Scale.X) ||
+                y < Buttons[i].S!.Position.Y || y > (Buttons[i].S!.Position.Y + Buttons[i].S!.Texture.Size.Y * Buttons[i].S!.Scale.Y)) 
                 continue;
             _selected = i;
-            if (_selected == -1 || Sliders?[_selected] == null || !isPressed) return _selected;
-            var scale = new Vector2f((x - Sliders[_selected]!.Position.X) / Sliders[_selected]!.Size.X, 1);
+            if (!Buttons[_selected].IsSlider || !isPressed) return _selected;
+            var scale = new Vector2f((x - Buttons[_selected].R!.Position.X) / Buttons[_selected].R!.Size.X, 1);
             scale.X = scale.X < 0 ? 0 : (scale.X > 1 ? 1 : scale.X);
-            Sliders![_selected]!.Scale = scale;
+            Buttons[_selected].R!.Scale = scale;
         }
 
-        return _selected;
+        return -1;
     }
 
     public float Slider
     {
         get
         {
-            if (_selected < 0 || Sliders?[_selected] == null)
+            if (_selected < 0 || Buttons?[_selected].R! == null)
                 return -1;
-            return Sliders[_selected]!.Scale.X;
+            return Buttons[_selected].R!.Scale.X;
         }
     }
 
@@ -47,9 +62,6 @@ internal class ButtonLayout: Drawable
         if (Buttons == null) return;
         foreach (var i in Buttons)
             target.Draw(i);
-        if (Sliders == null) return;
-        foreach (var i in Sliders)
-            if (i != null) target.Draw(i);
     }
 }
 
@@ -59,27 +71,28 @@ internal static class LayoutFactory
     {
         var layout = new ButtonLayout
         {
-            Buttons = new Sprite[4],
-            Sliders = new RectangleShape?[4]
+            Buttons = new Button[4]
         };
 
-        layout.Sliders[0] = new RectangleShape
+        for (var i = 0; i < 4; ++i)
+            layout.Buttons[i] = new Button
+            {
+                S = new Sprite
+                {
+                    Texture = Params.MenuButton,
+                    Scale = new Vector2f((0.5f * x) / Params.MenuButton.Size.X,
+                        (0.15f * y) / Params.MenuButton.Size.Y),
+                    Position = new Vector2f(x * 0.25f, y * (i + 1) / 6f)
+                }
+            };
+
+        layout.Buttons[0].IsSlider = true;
+        layout.Buttons[0].R = new RectangleShape
         { 
             Size = new Vector2f(x * 0.5f, 3), 
             FillColor = Color.Green, 
             Position = new Vector2f(0.25f * x, y / 6f)
         };
-
-        for (var i = 0; i < 4; ++i)
-        {
-            layout.Buttons[i] = new Sprite
-            {
-                Texture = Params.MenuButton,
-                Scale = new Vector2f((0.5f * x) / Params.MenuButton.Size.X,
-                    (0.15f * y) / Params.MenuButton.Size.Y),
-                Position = new Vector2f(x * 0.25f, y * (i + 1) / 6f)
-            };
-        }
 
         return layout;
     }
@@ -88,7 +101,8 @@ internal static class LayoutFactory
     {
         var layout = new ButtonLayout
         {
-            Buttons = new Sprite[4],
+            Buttons = new Button[4],
+            //Sliders = new RectangleShape?[4]
         };
 
         return layout;
